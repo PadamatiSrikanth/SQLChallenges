4f76c769983c9ba69a9c7d0e04b4c37bc5a2f175
PadamatiSrikanth <padamatisrikanthreddy39@gmail.com>
2022-09-29 07:58:08 +0530

    modified the challenge-1

--- Challenge-1/challenge1.sql
+++ Challenge-1/challenge1.sql
@@ -106,5 +106,15 @@
 		WHERE order_date < join_date
 		GROUP BY customer_id
 -- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+		SELECT 
+ 		customer_id,
+		--  s.product_id,
+		--  product_name,
+		--  price,
+		--  order_date,
+		SUM(IF(m.product_name = 'sushi', (price * 20),(price * 10))) points
+		FROM sales s
+		JOIN menu m ON s.product_id = m.product_id
+		GROUP BY customer_id	
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
 
